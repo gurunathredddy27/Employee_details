82d7ab28634ac9ad7cc406c7362bb8235bd2ae94
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-06 17:00:54 +0530

    added wage per hour

--- src/Emp_attendance.java
+++
@@ -1,5 +0,0 @@
-public class Emp_attendance {
-    public static void main(String[] args) {
-        System.out.println("Namaste");
-    }
-}
--- src/Employee.java
+++ src/Employee.java
@@ -4,35 +4,37 @@ import java.util.Scanner;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Namaste!");
-        int totalDays = 26;
+        int totalDays = 20;
+        int perHourRs = 20;
+        int fulDayHours = 8;
         Scanner sc = new Scanner(System.in);
+        String[] empName = {"Ram", "Sita", "Radha", "Krishna", "Shiva"};
+
 
         System.out.println("Enter your EmpId (101-105)");
         int empId = sc.nextInt();
 
         if (empId == 101) {
             System.out.println("Hello Ram");
-            int wageEmp = (1000 * totalDays);
+            int wageEmp = ((perHourRs * fulDayHours) * totalDays);
             System.out.println("your salary is " + wageEmp);
         }
         if (empId == 102) {
             System.out.println("Hello Sita");
-            int wageEmp = (750 * totalDays);
+            int wageEmp = ((perHourRs * fulDayHours) * totalDays);
             System.out.println("your salary is " + wageEmp);
         } else if (empId == 103) {
             System.out.println("Hello Radha");
-            int wageEmp = (1300 * totalDays);
+            int wageEmp = ((perHourRs * fulDayHours) * totalDays);
             System.out.println("your salary is " + wageEmp);
         } else if (empId == 104) {
             System.out.println("Hello Krishna");
-            int wageEmp = (1500 * totalDays);
+            int wageEmp = ((perHourRs * fulDayHours) * totalDays);
             System.out.println("your salary is " + wageEmp);
         } else if (empId == 105) {
             System.out.println("Hello Shiva");
-            int wageEmp = (1420 * totalDays);
+            int wageEmp = ((perHourRs * fulDayHours) * totalDays);
             System.out.println("your salary is " + wageEmp);
-        } else {
-            System.out.println("It is invalid \n Enter valid Employee ID");
         }
     }
 }
